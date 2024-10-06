a338246993ce9f43e9a6b7a18bf67aaaab6565a1
mohammed ramadi <taghreedforeducation2019@gmail.com>
2024-10-07 02:55:01 +0300

    LFS

---
+++ Firest_Project/CorrosionDetection/main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("../Yolo-Weights/yolov8l.pt")
+results = model ("Images/1.png", show=True)
+cv2.waitKey(0)
+
+classNames = ["cacat_korosi"]
+
---
+++ Firest_Project/Readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5b6c0ef014dd6d660f1cbcf10adecc51bc8b5315beb9c9aadbf46cf4ab271266
+size 5353
